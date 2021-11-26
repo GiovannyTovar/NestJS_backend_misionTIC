351fbe3c6f4463a4206b0fd9a51e1101bebec87b
GiovannyTovar <giovannytovar@outlook.com>
2021-11-26 00:55:52 -0500

    Actual. Controlador Producto / cambiar json por send

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, NotFoundException, Param, Post, Put, Res } from '@nestjs/common';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { ProductService } from './product.service';
 
@@ -10,48 +10,33 @@ export class ProductController {
     @Get()
     async getProducts(@Res() res) {
         const products = await this.productService.getProducts();
-        return res.status(HttpStatus.OK).json({
-            data: products
-        });
+        return res.status(HttpStatus.OK).send(products);
     }
 
     @Get(':product_id')
     async getProduct(@Res() res, @Param('product_id') product_id: number) {
         const product = await this.productService.getProductById(product_id);
-        return res.status(HttpStatus.OK).json({
-            data: product
-        });
+        return res.status(HttpStatus.OK).send(product);
     }
 
     @Post()
     async createProduct(@Res() res, @Body() createProductDTO: CreateProductDTO) {
         const product = await this.productService.createProduct(createProductDTO);
-        return res.status(HttpStatus.CREATED).json({
-            message: 'recived',
-            data: product
-        });
+        return res.status(HttpStatus.CREATED).send(product);
     }
 
     @Put(':product_id')
     async updateProduct(@Param('product_id') product_id: string, @Res() res, @Body() createProductDTO: CreateProductDTO) {
         const updatedProduct = await this.productService.updateProduct(product_id, createProductDTO);
-        return res.status(HttpStatus.OK).json({
-            message: 'Updated',
-            data: updatedProduct
-        })
+        return res.status(HttpStatus.OK).send(updatedProduct);
     }
 
     @Delete(':product_id')
     async deleteProduct(@Param('product_id') product_id: number, @Res() res) {
         const deletedProduct = await this.productService.deleteProduct(product_id);
         if(!deletedProduct){
-            return res.status(HttpStatus.NOT_FOUND).json({
-                message: 'Product Not Found'
-            })
+           throw new NotFoundException('Product not found');
         }
-        return res.status(HttpStatus.OK).json({
-            message: 'deleted',
-            data: deletedProduct
-        })
+        return res.status(HttpStatus.OK).send(deletedProduct);
     }
 }
